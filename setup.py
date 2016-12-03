003ce11f4ae0cd50cad8263db426c7658841c98c
Richard King <richrdkng@gmail.com>
2016-12-03 21:41:53 +0100

    Drop Python 2.6 support

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pytools_command',
 
-    version='0.0.4',
+    version='0.0.5',
 
     description='A collection of functions to run and evaluate external commands from Python.',
     long_description=long_description,
@@ -33,7 +33,6 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
