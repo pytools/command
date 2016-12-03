87467ed0e56d818ad983d317cd478314e05e4815
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-12-03 23:50:00 +0000

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pytools_command',
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='A collection of functions to run and evaluate external commands from Python.',
     long_description=long_description,
